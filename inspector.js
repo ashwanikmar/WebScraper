9ffd8c6380385d0c018f5a1fb94ac2531dbbd7d5
Ashwani Kumar <ashwani.ku@outlook.com>
2023-10-01 23:58:36 +0530

    added button click

--- inspector.js
+++ inspector.js
@@ -1,72 +1,180 @@
 var forLoopElements = [];
 var highlightedElements = [];
-document.addEventListener(
-  "mousemove",
-  function (e) {
-    let target = e.target;
+var highlightedButtonElements = [];
+var isListeningForLoopMousemove = false;
+var isListeningButtonSelectMousemove = false;
 
-    /**
-     * Step 1. We are selecting previous element and then next element.
-     * and matching with the target.
-     */
-    let el = target;
-    let loopElements = [el];
+var clickButtonElements = [];
+// var isListeningForLoopMousemove;
+forLoopInspectionStart = (e) => {
+  let target = e.target;
 
-    while (similarElementPredictionScore(el, el.previousElementSibling) > 80) {
-      el = el.previousElementSibling;
-      loopElements.unshift(el);
-    }
+  /**
+   * Step 1. We are selecting previous element and then next element.
+   * and matching with the target.
+   */
+  let el = target;
+  let loopElements = [el];
 
-    el = target;
-    while (similarElementPredictionScore(el, el.nextElementSibling) > 80) {
-      el = el.nextElementSibling;
-      loopElements.push(el);
-    }
+  while (similarElementPredictionScore(el, el.previousElementSibling) > 80) {
+    el = el.previousElementSibling;
+    loopElements.unshift(el);
+  }
 
-    /**
-     * Step 2. If we have more than 1 element having similar property then it 
-     * means that it's a loop.
-     */
-    if (loopElements.length > 1) {
-      forLoopElements = loopElements;
-    }
+  el = target;
+  while (similarElementPredictionScore(el, el.nextElementSibling) > 80) {
+    el = el.nextElementSibling;
+    loopElements.push(el);
+  }
 
-    /**
-     * When user goes inside the loop, then I am highlighting similar element
-     * by finding its path and setting its outline on all forLoop element
-     */
-    let forLoopToTargetElementPath = findDepthOfTargetElement(
-      target,
-      forLoopElements
-    );
+  /**
+   * Step 2. If we have more than 1 element having similar property then it
+   * means that it's a loop.
+   */
+  if (loopElements.length > 1) {
+    forLoopElements = loopElements;
+  }
 
-    // Before highlighting new element I am removing the outline from the old once
-    for (let index = 0; index < highlightedElements.length; index++) {
-      highlightedElements[index].style.outline = "none";
-    }
-    highlightedElements = [];
+  /**
+   * When user goes inside the loop, then I am highlighting similar element
+   * by finding its path and setting its outline on all forLoop element
+   */
+  let forLoopToTargetElementPath = findDepthOfTargetElement(
+    target,
+    forLoopElements
+  );
 
-    for (let index = 0; index < forLoopElements.length; index++) {
-      let el = forLoopElements[index];
-      let isElementFound = true;
+  // Before highlighting new element I am removing the outline from the old once
+  for (let index = 0; index < highlightedElements.length; index++) {
+    highlightedElements[index].style.outline = "none";
+  }
+  highlightedElements = [];
 
-      for (let index = 0; index < forLoopToTargetElementPath.length; index++) {
-        if (el.children[forLoopToTargetElementPath[index]] == undefined) {
-          isElementFound = false;
-          break;
-        }
-        el = el.children[forLoopToTargetElementPath[index]];
+  for (let index = 0; index < forLoopElements.length; index++) {
+    let el = forLoopElements[index];
+    let isElementFound = true;
+
+    for (let index = 0; index < forLoopToTargetElementPath.length; index++) {
+      if (el.children[forLoopToTargetElementPath[index]] == undefined) {
+        isElementFound = false;
+        break;
       }
-
-      if (isElementFound) {
-        el.style.outline = "dashed";
-        highlightedElements.push(el);
-      }
+      el = el.children[forLoopToTargetElementPath[index]];
     }
-  },
-  true
-);
 
+    if (isElementFound) {
+      el.style.outline = "dashed";
+      el.style.outlineColor = "black";
+      highlightedElements.push(el);
+    }
+  }
+};
+
+forLoopInspectionStop = (e) => {
+  document.removeEventListener("mousemove", forLoopInspectionStart, true);
+  document.removeEventListener("click", forLoopInspectionStop);
+  isListeningForLoopMousemove = false;
+};
+
+selectClickButtonStart = (e) => {
+  let target = e.target;
+
+  /**
+   * Step 1. We are selecting previous element and then next element.
+   * and matching with the target.
+   */
+  let el = target;
+
+  /**
+   * When user goes inside the loop, then I am highlighting similar element
+   * by finding its path and setting its outline on all forLoop element
+   */
+  let forLoopToTargetElementPath = findDepthOfTargetElement(
+    target,
+    forLoopElements
+  );
+
+  // Before highlighting new element I am removing the outline from the old once
+  for (let index = 0; index < highlightedButtonElements.length; index++) {
+    highlightedButtonElements[index].style.outline = "none";
+  }
+  highlightedButtonElements = [];
+
+  for (let index = 0; index < forLoopElements.length; index++) {
+    let el = forLoopElements[index];
+    let isElementFound = true;
+
+    for (let index = 0; index < forLoopToTargetElementPath.length; index++) {
+      if (el.children[forLoopToTargetElementPath[index]] == undefined) {
+        isElementFound = false;
+        break;
+      }
+      el = el.children[forLoopToTargetElementPath[index]];
+    }
+
+    if (isElementFound) {
+      el.style.outline = "dashed";
+      el.style.outlineColor = "blue";
+      highlightedButtonElements.push(el);
+    }
+  }
+};
+
+selectClickButtonStop = (e) => {
+  let target = e.target;
+  document.removeEventListener("mousemove", selectClickButtonStart, true);
+  document.removeEventListener("click", selectClickButtonStop);
+  isListeningButtonSelectMousemove = false;
+  console.log(target);
+  clickButtonElements.push(target);
+};
+
+runActions = () => {
+  alert("yuu");
+for (let index = 0; index < clickButtonElements.length; index++) {
+  let forLoopToTargetElementPath = findDepthOfTargetElement(
+    clickButtonElements[index],
+    forLoopElements
+  );
+
+  for (let index = 0; index < forLoopElements.length; index++) {
+    let el = forLoopElements[index];
+    let isElementFound = true;
+
+    for (let index = 0; index < forLoopToTargetElementPath.length; index++) {
+      if (el.children[forLoopToTargetElementPath[index]] == undefined) {
+        isElementFound = false;
+        break;
+      }
+      el = el.children[forLoopToTargetElementPath[index]];
+    }
+
+    if (isElementFound) {
+      el.click();
+    }
+  }
+
+}
+
+  // for (let index = 0; index < forLoopElements.length; index++) {
+  //   let el = forLoopElements[index];
+  //   let isElementFound = true;
+
+  //   for (let index = 0; index < forLoopToTargetElementPath.length; index++) {
+  //     if (el.children[forLoopToTargetElementPath[index]] == undefined) {
+  //       isElementFound = false;
+  //       break;
+  //     }
+  //     el = el.children[forLoopToTargetElementPath[index]];
+  //   }
+
+  //   if (isElementFound) {
+  //     el.style.outline = "dashed";
+  //     el.style.outlineColor = "black";
+  //     highlightedElements.push(el);
+  //   }
+  // }
+};
 /**
  * Calculating similarity score multiple factor like class, child count and name.
  * we can enhance this logic to get more accurate results. currently it will fail
@@ -75,7 +183,7 @@ document.addEventListener(
  * @param {*} secondElement
  * @returns Matching score
  */
-function similarElementPredictionScore(firstElement, secondElement) {
+similarElementPredictionScore = (firstElement, secondElement) => {
   let score = 0;
 
   if (!firstElement || !secondElement) {
@@ -98,7 +206,7 @@ function similarElementPredictionScore(firstElement, secondElement) {
   }
 
   return score;
-}
+};
 
 /**
  * Backtracing nested element to the loop
@@ -144,3 +252,101 @@ findDepthOfTargetElement = (targetElement, forLoopElement) => {
 
   return pathToTargetElement;
 };
+
+initExtension = () => {
+  let containerEle = document.createElement("div");
+  containerEle.style.position = "fixed";
+  containerEle.style.bottom = "0px";
+  containerEle.style.left = "calc(50vh / 2)";
+  containerEle.style.width = "100vh";
+  containerEle.style.height = "100px";
+  containerEle.style.zIndex = "100";
+  containerEle.style.backgroundColor = "#1f1e24";
+  containerEle.style.borderTopLeftRadius = "15px";
+  containerEle.style.borderTopRightRadius = "15px";
+  containerEle.style.display = "flex";
+  containerEle.style.alignItems = "center";
+  containerEle.style.justifyContent = "center";
+  containerEle.style.fontFamily = "sans-serif";
+
+  let loopButtonEle = document.createElement("div");
+  loopButtonEle.innerText = "select loop";
+  loopButtonEle.style.color = "white";
+  loopButtonEle.style.backgroundColor = "#434248";
+  loopButtonEle.style.height = "50%";
+  loopButtonEle.style.width = "50%";
+  loopButtonEle.style.border = "1px dashed white";
+  loopButtonEle.style.borderRadius = "5px";
+  loopButtonEle.style.display = "flex";
+  loopButtonEle.style.justifyContent = "center";
+  loopButtonEle.style.alignItems = "center";
+  loopButtonEle.onclick = (e) => {
+    e.stopPropagation();
+
+    if (!isListeningForLoopMousemove) {
+      document.addEventListener("click", forLoopInspectionStop);
+      document.addEventListener("mousemove", forLoopInspectionStart, true);
+      actionContainer.style.display = "flex";
+      loopButtonEle.style.display = "none";
+      isListeningForLoopMousemove = true;
+    }
+  };
+
+  let clickButtonAction = document.createElement("div");
+  clickButtonAction.innerText = "select Click Button";
+  clickButtonAction.style.color = "white";
+  clickButtonAction.style.backgroundColor = "#434248";
+  clickButtonAction.style.height = "50%";
+  clickButtonAction.style.width = "25%";
+  clickButtonAction.style.border = "1px dashed white";
+  clickButtonAction.style.borderRadius = "5px";
+  clickButtonAction.style.textAlign = "center";
+  clickButtonAction.onclick = (e) => {
+    e.stopPropagation();
+    if (!isListeningButtonSelectMousemove) {
+      document.addEventListener("click", selectClickButtonStop);
+      document.addEventListener("mousemove", selectClickButtonStart, true);
+      isListeningButtonSelectMousemove = true;
+    }
+  };
+
+  let inputButtonAction = document.createElement("div");
+  inputButtonAction.innerText = "select Input Button";
+  inputButtonAction.style.color = "white";
+  inputButtonAction.style.backgroundColor = "#434248";
+  inputButtonAction.style.height = "50%";
+  inputButtonAction.style.width = "25%";
+  inputButtonAction.style.border = "1px dashed white";
+  inputButtonAction.style.borderRadius = "5px";
+  inputButtonAction.style.textAlign = "center";
+
+  let runButtonAction = document.createElement("div");
+  runButtonAction.innerText = "Run";
+  runButtonAction.style.color = "white";
+  runButtonAction.style.backgroundColor = "#6664ff";
+  runButtonAction.style.height = "50%";
+  runButtonAction.style.width = "25%";
+  runButtonAction.style.border = "1px dashed white";
+  runButtonAction.style.borderRadius = "5px";
+  runButtonAction.style.textAlign = "center";
+  runButtonAction.id = "run_actions";
+  runButtonAction.onclick = runActions;
+
+  let actionContainer = document.createElement("div");
+  actionContainer.innerHTML = `<div> Add Action </>`;
+  actionContainer.style.display = "none";
+  actionContainer.style.width = "100%";
+  actionContainer.style.justifyContent = "space-around";
+  actionContainer.style.color = "white";
+
+  actionContainer.appendChild(clickButtonAction);
+  actionContainer.appendChild(inputButtonAction);
+  actionContainer.appendChild(runButtonAction);
+
+  containerEle.appendChild(loopButtonEle);
+  containerEle.appendChild(actionContainer);
+
+  document.body.appendChild(containerEle);
+};
+
+initExtension();
